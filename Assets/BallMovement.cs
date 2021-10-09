07a9193af91a766eb8722b1607239433c360c8ef
Mücahit Eren Özkur <mucahiterenozkur@hotmail.com>
2021-10-09 17:21:38 +0300

    level designs completed

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -24,11 +24,22 @@ public class BallMovement : MonoBehaviour
     {
         //rb.AddForce(Vector3.up * jumpForce);
         rb.velocity = Vector3.up * jumpForce;
-        GameObject splashEffect = Instantiate(splashEffectPrefab, transform.position + new Vector3(0f,-0.24f,0f), transform.rotation);
+        GameObject splashEffect = Instantiate(splashEffectPrefab, transform.position + new Vector3(0f,-0.19f,0f), transform.rotation);
         splashEffect.transform.SetParent(other.gameObject.transform);
         Destroy(splashEffect, 1f);
 
-        string tagName = other.gameObject.tag;
+        //string tagName = other.gameObject.tag;
+        //Debug.Log(tagName);
+
+        if(other.gameObject.CompareTag("Unsafe"))
+        {
+            Debug.Log("dead");
+        }
+
+        //if(tagName == "Unsafe Block")
+        //{
+        //    Debug.Log("dead");
+        //}
     }
 
     
